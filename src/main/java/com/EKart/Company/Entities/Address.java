ac9dabcb0b75ac1dba8e2bb43bb56ed115e14456
Ankur <ankur22051998@gmail.com>
2025-08-16 12:14:02 +0530

    Added Address Entitiy

---
+++ src/main/java/com/EKart/Company/Entities/Address.java
@@ -0,0 +1,13 @@
+package com.EKart.Company.Entities;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Address {
+
+	public Address() {
+		// TODO Auto-generated constructor stub
+	}
+	private String  house_number;
+
+}
