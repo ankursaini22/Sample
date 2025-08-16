6a305e97e54d96fc0efc4922ce89936c4fbcb861
Ankur <ankur22051998@gmail.com>
2025-08-16 12:18:39 +0530

    Added Address Entitiy

--- src/main/java/com/EKart/Company/Entities/Address.java
+++ src/main/java/com/EKart/Company/Entities/Address.java
@@ -8,6 +8,19 @@ public class Address {
 	public Address() {
 		// TODO Auto-generated constructor stub
 	}
-	private String  house_number;
+	private String  number;
+	public String getNumber() {
+		return number;
+	}
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	private String Street;
+	public String getStreet() {
+		return Street;
+	}
+	public void setStreet(String street) {
+		Street = street;
+	}
 
 }
